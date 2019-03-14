0a6089beb7191370cb49954fde72525877afe4cc
Adam Jensen <acjensen@gmail.com>
2019-03-14 06:36:48 -0400

    Bump version to 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExProgress.Mixfile do
   def project do
     [
       app: :ex_progress,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
